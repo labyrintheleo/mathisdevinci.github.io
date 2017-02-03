9a59b1494efd73e333162e24f620911b73337b16
MathisDeVinci <mathis.joseph@devinci.fr>
2017-02-03 14:55:41 +0100

    Update ajax1.js

--- ajax1.js
+++ ajax1.js
@@ -1,14 +1,14 @@
 function ajax()
 {
 	var xhr=NULL;
-	/*if (window.XMLHttpRequest)
+	if (window.XMLHttpRequest)
 	{
 		xhr= new XMLHttpRequest ();
 	}
 	else if (window.ActiveXObject)
 	{
 		xhr = new ActiveXObject("Microsoft.XMLHTTP");
-	}*/
+	}
 
 xhr.open("GET", "mubiz.com/developers/blocks", false);
 xhr.send(null):=;
