0bdae1e67ea3d6f104baef05a0b9d9fd2bfc198c
MathisDeVinci <mathis.joseph@devinci.fr>
2017-03-10 13:49:04 +0100

    Update bitcoin.js

--- bitcoin.js
+++ bitcoin.js
@@ -24,3 +24,63 @@ $(document).ready(function() {
 		}
 	});
 });
+
+$(document).ready(function() {
+	$.ajax({
+		url : "http://bitcoin.mubiz.com/blockchaininfo",
+		dataType : "json",
+		contentType : "application/json; charset=utf-8",
+		type : "GET",
+		timeout:	"5000",
+		async : false,
+
+		success : function(data) {
+			$('#bitcoin_block_number').append(data.height);
+		},
+
+		error : function(xhr, status, err) {
+			$('#bitcoin_block_number').append(err+" N/A");
+		}
+	});
+});
+
+$(document).ready(function() {
+	$.ajax({
+		url : "http://bitcoin.mubiz.com/mininginfo",
+		dataType : "json",
+		contentType : "application/json; charset=utf-8",
+		type : "GET",
+		timeout:	"5000",
+		async : false,
+
+		success : function(data) {
+			$('#bitcoin_block_number').append(data.height);
+		},
+
+		error : function(xhr, status, err) {
+			$('#bitcoin_block_number').append(err+" N/A");
+		}
+	});
+});
+
+$(document).ready(function() {
+	$.ajax({
+		url : "http://bitcoin.mubiz.com/peerinfo",
+		dataType : "json",
+		contentType : "application/json; charset=utf-8",
+		type : "GET",
+		timeout:	"5000",
+		async : false,
+
+		success : function(data) {
+			$('#bitcoin_block_number').append(data.height);
+		},
+
+		error : function(xhr, status, err) {
+			$('#bitcoin_block_number').append(err+" N/A");
+		}
+	});
+});
+
+
+
