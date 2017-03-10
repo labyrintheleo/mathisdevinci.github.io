379b1a23264a978c2c43d23d242f5c0a424c8bdd
MathisDeVinci <mathis.joseph@devinci.fr>
2017-03-10 14:23:20 +0100

    Add files via upload

--- bitcoin.js
+++ bitcoin.js
@@ -6,6 +6,9 @@
  * 
  */
 
+
+
+
 $(document).ready(function() {
 	$.ajax({
 		url : "https://api.blockcypher.com/v1/btc/main",
@@ -32,53 +35,21 @@ $(document).ready(function() {
 		timeout:	"5000",
 		async : false,
 
-		success : function(data2) {
-			$('#bitcoin_connection').append(data2.connections);
+		success : function(data) {
+			$('#bitcoin_connection').append(data.connections);
+			$('#bitcoin_version').append(data.version);
+			$('#bitcoin_protocole_version').append(data.protocolversion);
 		},
 
 		error : function(xhr, status, err) {
 			$('#bitcoin_connection').append(err+" N/A");
+			$('#bitcoin_version').append(err+" N/A");
+			$('#bitcoin_protocole_version').append(err+" N/A");
 		}
 	});
+
 });
 
 
-$(document).ready(function() {
-	$.ajax({
-		url : "http://bitcoin.mubiz.com/mininginfo",
-		dataType : "json",
-		contentType : "application/json; charset=utf-8",
-		type : "GET",
-		timeout:	"5000",
-		async : false,
-
-		success : function(data) {
-			$('#bitcoin_block_number').append(data.height);
-		},
-
-		error : function(xhr, status, err) {
-			$('#bitcoin_block_number').append(err+" N/A");
-		}
-	});
-});
-
-$(document).ready(function() {
-	$.ajax({
-		url : "http://bitcoin.mubiz.com/peerinfo",
-		dataType : "json",
-		contentType : "application/json; charset=utf-8",
-		type : "GET",
-		timeout:	"5000",
-		async : false,
-
-		success : function(data) {
-			$('#bitcoin_block_number').append(data.height);
-		},
-
-		error : function(xhr, status, err) {
-			$('#bitcoin_block_number').append(err+" N/A");
-		}
-	});
-});
 
 
