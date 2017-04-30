3690f5793ff120610e320f96e270170994e02fcc
MathisDeVinci <mathis.joseph@devinci.fr>
2017-04-30 13:53:37 +0200

    Update ajax1.js

--- ajax1.js
+++ ajax1.js
@@ -1,16 +1,32 @@
-function ajax()
-{
-	var xhr=NULL;
-	if (window.XMLHttpRequest)
-	{
-		xhr= new XMLHttpRequest ();
-	}
-	else if (window.ActiveXObject)
-	{
-		xhr = new ActiveXObject("Microsoft.XMLHTTP");
-	}
+/**
+ * @author	Albin CAUDERLIER
+ * @date	24/02/2017
+ * 
+ * Script jQuery appelant l'API de BlockCypher.com et affichant les données.
+ * 
+ */
 
-xhr.open("GET", "mubiz.com/developers/blocks", false);
-xhr.send(null):=;
 
-alert (xhr.responseText);
+$(document).ready(function(){
+ $("button").click(function(e) {
+     e.preventDefault();
+     $.ajax({
+		url : "https://api.mubiz.com/"+document.GetElementById('inputadresse').value+"/bitcoin/",
+		dataType : "json",
+		contentType : "application/json; charset=utf-8",
+		type : "GET",
+		timeout:	"5000",
+		async : false,
+
+		success : function(data) {
+			$('#user_url').append(data.user_url);
+			$('#user_name').append(data.user_name);
+		},
+
+		error : function(xhr, status, err) {
+			$('#user_url').append(err+" N/A");
+			$('#user_adresse').append(err+" N/A");
+		}
+	});
+ });	
+});
