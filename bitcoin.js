54798be6ff873b668151dfb0c088ae461373c3c4
MathisDeVinci <mathis.joseph@devinci.fr>
2017-04-30 13:54:06 +0200

    Update bitcoin.js

--- bitcoin.js
+++ bitcoin.js
@@ -59,28 +59,5 @@ $(document).ready(function() {
 
 });
 
-$(document).ready(function(){
- $("button").click(function(e) {
-     e.preventDefault();
-     $.ajax({
-		url : "https://api.mubiz.com/"+document.GetElementById('inputadresse').value+"/bitcoin/",
-		dataType : "json",
-		contentType : "application/json; charset=utf-8",
-		type : "GET",
-		timeout:	"5000",
-		async : false,
-
-		success : function(data) {
-			$('#user_url').append(data.user_url);
-			$('#user_name').append(data.user_name);
-		},
-
-		error : function(xhr, status, err) {
-			$('#user_url').append(err+" N/A");
-			$('#user_adresse').append(err+" N/A");
-		}
-	});
- });	
-});
 		  
 
